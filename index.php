8711bc363b794693f463ba8ed652766fcad5b7af
nommy <win.back.0@gmail.com>
2019-09-17 10:09:05 +0800

    heroku

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題</title>
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/jtc.css">
+</head>
+<body>
+
+    <div class="header">
+      <div class="header-left">JAPANESE TEA .com</div>
+      <ul>
+        <li>
+          <div class="dropdown mx-auto" style="text-align:right;">
+            　<a class="btn btn-white dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" w-auto>
+              MENU
+    　　　　　</a>
+
+            　<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              　<a class="dropdown-item" href="http://www.ocha.tv/history/japanese_tea_history/">日本茶の歴史</a>
+              　<a class="dropdown-item" href="http://www.ocha.tv/varieties/nihoncha_varieties/">種類</a>
+              　<a class="dropdown-item" href="https://www.jetro.go.jp/biz/areareports/2019/f8d95837fba64c4d.html">日本茶市場</a>
+              　<a class="dropdown-item" href="http://www.ocha.tv/how_to_brew/nihoncha/">淹れ方</a>
+            　</div>
+          </div>
+        </li>
+      </ul>
+
+
+
+
+
+    <div class="main">
+      <div class="top">
+          <div class="sub">  Do you know Japanese tea ?</div>
+          <div class="name"> JAPANESE TEA .com</div>
+      </div>
+    </div>
+
+
+
+      <div class="btn-wrapper">
+          <div id="js-hover-btn" class="btn_direction" a > ⇣</div>
+      </div>
+
+
+
+      <h2 class="section-title">Menu</h2>
+
+
+     <!--  <div class="contents-item">
+            <p>日本茶の歴史</p>
+
+
+        <div class="contents-item">
+            <p>種類</p>
+        </div>
+
+
+        <div class="contents-item">
+            <p>日本茶の市場</p>
+        </div>
+
+        <p>淹れ方</p>
+        <div class="contents-item">
+
+        </div>
+      </div>
+      -->
+
+        <div class="contentainer">
+          <div class="row p-3 justify-content-around">
+            <div class="col-2">
+                <img src="assets/img/history.jpg" alt="日本茶の歴史">
+            </div>
+            <div class="col-2">
+                <img src="assets/img/kinds.jpg" alt="種類">
+            </div>
+            <div class="col-2">
+                <img src="assets/img/money.jpg" alt="日本茶の市場">
+            </div>
+            <div class="col-2">
+                <img src="assets/img/tea.jpg" alt="淹れ方">
+            </div>
+          </div>
+        </div>
+
+
+    <div class="contentainer">
+       <div class="row p-3 justify-content-around">
+          <div class="col-2 border border-white">
+              紀元前の古代、中国で発見されたといわれるお茶。漢の時代には、すでにお茶はよく知られていたようです。
+          </div>
+          <div class="col-2 border border-white">
+              お茶は飲用するものだけでなく、直接摂取するものもあります。また、飲用茶も発酵度によって細かく分類されます。
+          </div>
+          <div class="col-2 border border-white">
+              国内の緑茶飲料の課題は、市場規模が20年前と比較して約4倍に伸長する一方で、茶農家の数や茶園の栽培面積が減少しているため、国内の緑茶生産量は減少傾向が続いている。
+          </div>
+          <div class="col-2 border border-white">
+              1煎目,最後の1滴までしぼりきります。2煎目は1煎目よりやや高温の湯を使用すると、茶葉に残ったうま味と高温で溶け出す適度な渋味が楽しめます。2煎目からは早めに注ぎ分けてください。
+          </div>
+        </div>
+    </div>
+
+      <div class="contentainer">
+        <div class="row　p-3 justfiy-content-around">
+          <div class="col-2">
+              <button type="button" class="abc btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                  　　　　　　　   日本茶の歴史
+                 　　　　　　　　</button>
+                   　　　　<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                   　　　　  <div class="modal-dialog" role="document">
+                       　　　　<div class="modal-content">
+                         　　　　<div class="modal-header">
+                           　　　　<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                           　　　
+                   　　　　　　　  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                           　　　　　<span aria-hidden="true">&times;</span>
+                         　　　　　</button>
+                       　　　　　</div>
+                       　　　　 　　 <div class="modal-body">
+                         　　　　 　　　 ...
+                       　　　　 　　　</div>
+
+                         　　　　　　<div class="modal-footer">
+                           　　　　　　<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          　　　　　　 <button type="button" class="btn btn-primary">Save changes</button>
+                       　　　　　　  </div>
+                     　　　　　  </div>
+                    　　　　 </div>
+                   　　　　</div>
+          </div>
+          <div class="col-2">
+              <button type="button" class="abc btn-white" data-toggle="modal" data-target="#exampleModalLong">
+                  　　　　　　　   種類
+                 　　　　　　　　</button>
+                   　　　　<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                   　　　　  <div class="modal-dialog" role="document">
+                       　　　　<div class="modal-content">
+                         　　　　<div class="modal-header">
+                           　　　　<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                           　　　
+                   　　　　　　　  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                           　　　　　<span aria-hidden="true">&times;</span>
+                         　　　　　</button>
+                       　　　　　</div>
+                       　　　　 　　 <div class="modal-body">
+                         　　　　 　　　 ...
+                       　　　　 　　　</div>
+
+                         　　　　　　<div class="modal-footer">
+                           　　　　　　<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          　　　　　　 <button type="button" class="btn btn-primary">Save changes</button>
+                       　　　　　　  </div>
+                     　　　　　  </div>
+                    　　　　 </div>
+                   　　　　</div>
+          </div>
+          <div class="col-2">
+              <button type="button" class="abc btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                  　　　　　　　   日本の市場
+                 　　　　　　　　</button>
+                   　　　　<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                   　　　　  <div class="modal-dialog" role="document">
+                       　　　　<div class="modal-content">
+                         　　　　<div class="modal-header">
+                           　　　　<h5 class="modal-title" id="exampleModalLongTitle">日本茶の歴史</h5>
+                           　　　
+                   　　　　　　　  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                           　　　　　<span aria-hidden="true">&times;</span>
+                         　　　　　</button>
+                       　　　　　</div>
+                       　　　　 　　 <div class="modal-body">
+                       　　　　 　　　</div>
+                         　　　　　　<div class="modal-footer">
+                           　　　　　　<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          　　　　　　 <button type="button" class="btn btn-primary">Save changes</button>
+                       　　　　　　  </div>
+                     　　　　　  </div>
+                    　　　　 </div>
+                   　　　　</div>
+          </div>
+          <div class="col-2">
+              <button type="button" class="abc btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                  　　　　　　　   淹れ方
+                 　　　　　　　　</button>
+                   　　　　<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                   　　　　  <div class="modal-dialog" role="document">
+                       　　　　<div class="modal-content">
+                         　　　　<div class="modal-header">
+                           　　　　<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                           　　　
+                   　　　　　　　  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                           　　　　　<span aria-hidden="true">&times;</span>
+                         　　　　　</button>
+                       　　　　　</div>
+                       　　　　 　　 <div class="modal-body">
+                         　　　　 　　　 afdsf
+                       　　　　 　　　</div>
+
+                         　　　　　　<div class="modal-footer">
+                           　　　　　　<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          　　　　　　 <button type="button" class="btn btn-primary">Save changes</button>
+                       　　　　　　  </div>
+                     　　　　　  </div>
+                    　　　　 </div>
+                   　　　　</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+          <img src="assets/img/asia.jpg" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" >Go somewhere</a>
+          </div>
+       </div>
+
+
+
+
+      <div class="btn-wrapper">
+          <a class="btn js-scroll">⇡</a>
+      </div>
+
+
+    <div class="footer">
+      <div class="footer-logo">Copyright © teateatea Co.,Ltd. </div>
+      <div class="footer-list">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="assets/js/jtc.js"></script>
+
+
+
+ </body>
+</html>
+
